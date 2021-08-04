639e365c212b5df9977ebb99a3e6683996db3cf0
Carmi Rothberg <crothber@brandeis.edu>
2021-08-03 22:13:00 -0400

    working run.py

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ SECONDS_PER_PLAYER = 900
 os.environ['STOCKFISH_EXECUTABLE'] = os.path.dirname(os.path.realpath(__file__)) + '/stockfish/stockfish_14_x64_avx2.exe'
 game = LocalGame(SECONDS_PER_PLAYER)
 
-winner_color, win_reason, history = play_local_game(gnash_bot(), trout_bot(), game=game)
-history.save('gnash/games/')
+winner_color, win_reason, history = play_local_game(gnash_bot.GnashBot(), trout_bot.TroutBot(), game=game)
+history.save('games/game.json')
