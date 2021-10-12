8694b11ba875900fe1b4e78cdd17874f7ce4ada2
Carmi Rothberg <crothber@brandeis.edu>
2021-10-12 15:53:13 -0400

    background_update kind of working now

--- background_update.py
+++ background_update.py
@@ -1,17 +1,77 @@
 import os
+# os.environ['FORKED_BY_MULTIPROCESSING'] = '1'
+
 import chess
 import flask
 
-from flask import Flask, request
+from flask import Flask, request, url_for
+from celery import Celery
 
-from gnash_bot import GnashBot
+# from gnash_bot import GnashBot
 import json
 import time
 
+'''
+SETUP INSTRUCTIONS:
+1. pip install celery, flask, gevent
+2. Download redis (https://github.com/microsoftarchive/redis/releases/tag/win-3.0.504)
+3. Start a redis server (redis-server.exe)
+4. Run `celery -A background_update.celery worker -l info -P gevent`
+5. Run `python background-update.py`
+6. You can now access background processes on the server
+'''
+
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, backend=app.config['RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+@celery.task(bind=True)
+def long_task(self):
+    for i in range(15):
+        time.sleep(1)
+        self.update_state(state='PROGRESS', meta={'current': i, 'total': 15, 'status': 'message here'})
+    return {'current': 15, 'total': 15, 'status': 'Task completed!', 'result': 42}
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        # job did not start yet
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return json.dumps(response)
+
+@app.route('/longtask', methods=['GET'])
+def longtask():
+    task = long_task.apply_async()
+    return flask.Response(json.dumps({}), 202, {'Location': url_for('taskstatus', task_id=task.id)})
 
 all_games = {}
-
+"""
 class Game:
     def __init__(self, color, board, oppName, game_id):
         self.bot = GnashBot()
@@ -106,6 +166,6 @@ def destroy_game():
     game_id = request.args.get('game_id')
     del all_games[game_id]
     return flask.Response(status=200)
-
+"""
 if __name__ == '__main__':
     app.run(debug=True)
