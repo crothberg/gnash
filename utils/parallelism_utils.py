f1e73c09d3c72ac6dd0f326ad007265c7a9ce6a1
eitanrothberg <rothberg.10@osu.edu>
2021-10-17 22:48:08 -0400

    comments

--- utils/parallelism_utils.py
+++ utils/parallelism_utils.py
@@ -1,8 +1,10 @@
-# from multiprocessing import Pool
-from multiprocessing.dummy import Pool
+#Uncomment for multiprocessing
+from multiprocessing import Pool, cpu_count
+POOL_SIZE = cpu_count()-1
 
-POOL_SIZE = 100 #mp.cpu_count()-1
-# ^NOTE: POOL_SIZE=100 works for threading, but is too big for multiprocessing
+# #Uncomment for threading
+# from multiprocessing.dummy import Pool
+# POOL_SIZE = 100
 
 def chunks(lst, chunkSize=POOL_SIZE):
     for i in range(0, len(lst), chunkSize):
