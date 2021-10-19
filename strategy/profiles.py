2bee7e51e2de3efda762023e2ce7d7d41eb727b4
eitanrothberg <rothberg.10@osu.edu>
2021-10-19 14:07:51 -0400

    update profiles

--- strategy/profiles.py
+++ strategy/profiles.py
@@ -3,15 +3,15 @@ from os import name
 
 profiles = {
     #us and them
-    "oracle": (.1, .1),
+    "oracle": (.1, .1), #this seems to be very solid
     "random": (1.0, None),
     "RandomBot": (1.0, None),
     "attacker": (.1, None),
     "AttackBot": (.1, None),
     "penumbra": (.7, .85),
     "Fianchetto": (.8, .1),
-    "StrangeFish2": (.7, 0),
-    "trout": (None, None),
+    "StrangeFish2": (.7, 0), #wins some loses some, good enough for now
+    "trout": (1, 0), #this is solid.
     "TroutBot": (None, None),
     "default": (.7, .5)
 }
